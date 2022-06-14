a4a138b1cdd0e7643c0cddb69bc2e7896f5ae61e
Michel Tüscher <mischu.tuescher@hotmail.com>
2022-06-14 17:56:57 +0200

    Add tracking

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function setCoordText(latitude, longitude) {
+const setCoordText = (latitude, longitude) => {
     let lat = document.getElementById('lat-container');
     let long = document.getElementById('long-container');
 
@@ -6,16 +6,30 @@ function setCoordText(latitude, longitude) {
     long.innerText = 'Long: ' + longitude;
 }
 
+const addTracking = () => {
+    navigator.geolocation.watchPosition(updatePosition, handleError, {enableHighAccuracy: true})
+}
+
+const updatePosition = position => {
+    const latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+    console.log('new Pos: ', latLng);
+    window.myMarker.setLatLng(latLng);
+}
+
 const setUpMap = position => {
     setCoordText(position.coords.latitude, position.coords.longitude);
+    const latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+    console.log('init Pos: ', latLng);
 
     let map = L.map('leaflet-map');
-    map.setView([position.coords.latitude, position.coords.longitude], 15);
+    map.setView(latLng, 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
     }).addTo(map);
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    window.myMarker = L.marker(latLng).addTo(map);
+
+    addTracking();
 }
 
 const handleError = () => {
@@ -25,15 +39,11 @@ const handleError = () => {
 if ("geolocation" in navigator) {
    const posBtn = document.getElementById('position-btn');
 
-   const geoOptions = {
-       enableHighAccuracy: true,
-   }
-
    posBtn.addEventListener('click', ev => {
        ev.preventDefault();
        posBtn.disabled = true;
 
-       navigator.geolocation.getCurrentPosition(setUpMap, handleError, geoOptions);
+       navigator.geolocation.getCurrentPosition(setUpMap, handleError,{enableHighAccuracy: true});
    })
 } else {
     alert('Geolocation doesn\'t work in your current browser!');
