6d5090940562d5265b3f78c0894f792c9a52e139
Michel <mischu.tuescher@hotmail.com>
2022-06-21 16:33:24 +0200

    change some stuff

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-const setCoordText = (latitude, longitude) => {
+const setPositionDatas = position => {
     let lat = document.getElementById('lat-container');
     let long = document.getElementById('long-container');
+    let speed = document.getElementById('speed-container');
 
-    lat.innerText = 'Lat: ' + latitude;
-    long.innerText = 'Long: ' + longitude;
+    lat.innerText = 'Lat: ' + position.coords.latitude;
+    long.innerText = 'Long: ' + position.coords.longitude;
+    speed.innerText = 'Speed: ' + position.coords.speed;
 }
 
-const addTracking = () => {
-    console.log('Add position tracker');
-    navigator.geolocation.watchPosition(updatePosition, handleError, {enableHighAccuracy: true})
+const startTracking = () => {
+    console.log('Start position tracker');
+    return navigator.geolocation.watchPosition(updatePosition, handleError, {enableHighAccuracy: true})
+}
+
+const stopTracking = watchId => {
+    console.log('Stop position tracker');
+    navigator.geolocation.clearWatch(watchId);
 }
 
 const updatePosition = position => {
+    setPositionDatas(position);
     const latLng = L.latLng(position.coords.latitude, position.coords.longitude);
     console.log('new Pos: ', latLng);
     window.myMarker.setLatLng(latLng);
 }
 
 const setUpMap = position => {
-    setCoordText(position.coords.latitude, position.coords.longitude);
+    console.log('setup map');
+    setPositionDatas(position);
     const latLng = L.latLng(position.coords.latitude, position.coords.longitude);
     console.log('init Pos: ', latLng);
 
@@ -30,7 +39,7 @@ const setUpMap = position => {
     }).addTo(map);
     window.myMarker = L.marker(latLng).addTo(map);
 
-    addTracking();
+    document.getElementById('tracking-btn').disabled = false;
 }
 
 const handleError = () => {
@@ -39,13 +48,29 @@ const handleError = () => {
 
 if ("geolocation" in navigator) {
    const posBtn = document.getElementById('position-btn');
+   const trackingBtn = document.getElementById('tracking-btn');
+   let watchId = null;
 
    posBtn.addEventListener('click', ev => {
+       console.log('Pos Btn clicked');
        ev.preventDefault();
        posBtn.disabled = true;
 
        navigator.geolocation.getCurrentPosition(setUpMap, handleError,{enableHighAccuracy: true});
-   })
+   });
+
+   trackingBtn.addEventListener('click', ev => {
+       ev.preventDefault();
+
+       if (!watchId) {
+          watchId = startTracking();
+          trackingBtn.textContent = 'Stop position tracking';
+       } else {
+           stopTracking(watchId);
+           watchId = null;
+           trackingBtn.textContent = 'Start position tracking';
+       }
+   });
 } else {
     alert('Geolocation doesn\'t work in your current browser!');
 }
